7e825d146fb81b34f1b16c77c2c160ef5e532d0b
Tatiana <tatigerzens@gmail.com>
2024-05-01 21:57:59 -0300

    added json

--- domo/src/app/layout.js
+++ domo/src/app/layout.js
@@ -13,7 +13,7 @@ const inter = Inter({ subsets: ["latin"] });
 
 export const metadata = {
   title: "Demo Domo Test",
-  description: "Next app",
+  description: "",
 };
 
 export default function RootLayout({ children }) {
